cdaaa1b6e3f571fecdd0e6c8074514d81038725d
MohamedMunthasir <mohamedmunthasirm@gmail.com>
2023-05-30 17:10:52 +0000

    Success

--- dataproject/myapp/models.py
+++ dataproject/myapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib import admin
 # Create your models here.
+class Student(models.Model):
+    studentname = models.CharField(max_length=100,primary_key= True)
+    refno = models.CharField(max_length=100)
+    lastname= models.CharField(max_length=80)
+    emailid = models.EmailField()
+    age=models.CharField(max_length=10)
+    mobile=models.CharField(max_length=10)
+
+    
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('studentname','refno','lastname','emailid','age','mobile')
